c200bd29ecf3cb5a88db9a79c3d814785f8dfafa
Hidetaka Okamoto <info@wp-kyoto.net>
2020-04-23 12:58:47 +0900

    feat: pull to refresh

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
+import { RefresherEventDetail } from '@ionic/core';
 import Layout from '../components/Layouts'
 import config from '../config'
 import ListPost from '../components/ListPosts'
 import {
     WPPost
 } from '../wp.interface'
+import { IonRefresher, IonRefresherContent } from '@ionic/react'
 
 const Home: React.FC = () => {
     const [posts, setPosts] = useState<WPPost[]>([])
@@ -13,8 +15,18 @@ const Home: React.FC = () => {
             setPosts(data)
         })
     }, [])
+    const handleRefresh = useCallback((event: CustomEvent<RefresherEventDetail>) => {
+        config.wpClient.posts().perPage(30).then(data => {
+            setPosts(data)
+        }).finally(() => {
+            event.detail.complete()
+        })
+    }, [])
     return (
         <Layout name="Home">
+            <IonRefresher slot="fixed" onIonRefresh={handleRefresh}>
+                <IonRefresherContent />
+            </IonRefresher>
             <ListPost posts={posts} />
         </Layout>
     )
